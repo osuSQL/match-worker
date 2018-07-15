dc41e89db4a501372b70a7e7786709417e346c93
Hugo "ThePooN" Denizart <thepoon@cartooncraft.fr>
2018-07-15 09:44:56 +0200

    Simplify MatchWorkerConfig osu! structure

--- src/MatchWorkerConfig.ts
+++ src/MatchWorkerConfig.ts
@@ -1,17 +1,9 @@
+import { BanchoClientOptions } from "bancho.js";
 import * as fs from "fs";
 import * as _ from "lodash";
 
 export class MatchWorkerConfig {
-    public osu: {
-        username: "",
-        password: "",
-        host: "",
-        port: 0,
-        apiKey: "",
-        limiterTimespan: 0,
-        limiterPrivate: 0,
-        limiterPublic: 0,
-    };
+    public osu: BanchoClientOptions;
 
     constructor(load: boolean = true) {
         if(load)
